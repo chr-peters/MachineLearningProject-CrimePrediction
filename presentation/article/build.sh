2ef22ad6f41b1d4af02888cea49259ad88940320
cxan96 <c.p.xanten@gmail.com>
2017-11-07 07:56:21 +0100

    set up latex

---
+++ presentation/article/build.sh
@@ -0,0 +1,9 @@
+#! /bin/sh
+
+# set and create output directory
+OUTPUT_DIR=latex_output
+mkdir $OUTPUT_DIR
+
+# compile twice
+pdflatex -output-directory="$OUTPUT_DIR" main.tex
+pdflatex -output-directory="$OUTPUT_DIR" main.tex
