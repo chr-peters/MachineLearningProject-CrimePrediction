7aca448fe22a7c3fdcfa6bea0ae00322d54b22a8
cxan96 <c.p.xanten@gmail.com>
2017-11-07 08:02:15 +0100

    only compile twice when successful once

--- presentation/article/build.sh
+++ presentation/article/build.sh
@@ -5,5 +5,5 @@ OUTPUT_DIR=latex_output
 mkdir $OUTPUT_DIR
 
 # compile twice
-pdflatex -output-directory="$OUTPUT_DIR" main.tex
-pdflatex -output-directory="$OUTPUT_DIR" main.tex
+pdflatex -output-directory="$OUTPUT_DIR" main.tex && pdflatex -output-directory="$OUTPUT_DIR" main.tex
+
