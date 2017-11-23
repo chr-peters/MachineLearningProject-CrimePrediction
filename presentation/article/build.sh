baba520ff99590c45a3af159fa5db6d0d4f9ae45
cxan96 <c.p.xanten@gmail.com>
2017-11-23 07:51:41 +0100

    -p eingefuegt

--- presentation/article/build.sh
+++ presentation/article/build.sh
@@ -4,7 +4,7 @@ MAIN_FILE="Group13 Project Report.tex"
 
 # set and create output directory
 OUTPUT_DIR=latex_output
-mkdir $OUTPUT_DIR
+mkdir -p $OUTPUT_DIR
 
 # compile twice when successful once
 pdflatex -output-directory="$OUTPUT_DIR" "$MAIN_FILE" && pdflatex -output-directory="$OUTPUT_DIR" "$MAIN_FILE"
