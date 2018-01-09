1fafe9db8f4f4b8bc3affed96fdb87f3619071ec
cxan96 <c.p.xanten@gmail.com>
2018-01-09 16:56:02 +0100

    added build script for presentations

---
+++ presentation/slides/build.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+MAIN_FILE="Group13 Project Presentation"
+
+# set and create output directory
+OUTPUT_DIR=latex_output
+mkdir -p $OUTPUT_DIR
+
+# compile twice when successful once
+pdflatex -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex" && pdflatex -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex"
+biber "$OUTPUT_DIR/$MAIN_FILE"
+pdflatex -output-directory="$OUTPUT_DIR" "$MAIN_FILE.tex"
+
